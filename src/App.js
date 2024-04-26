c853d5007a832db117b3e4adc05d82a0f1df0ba9
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-04-27 00:19:40 +0530

    Routing watch video

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Body from './components/Body';
 import Head from './components/Head';
 import { AppContext } from './utils/SidebarContext';
+import MainContainer from './components/MainContainer';
+import WatchVideo from './components/WatchVideo';
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children:[
+      {
+        path: '/',
+        element: <MainContainer />
+      },
+      {
+        path:'watch',
+        element: <WatchVideo />
+      }
+    ]
+  }
+])
 const App = () => {
   return (
     <AppContext>
       <div className="h-screen">
           <Head />
-          <Body />
+          <RouterProvider router={appRouter} />
       </div>
     </AppContext>
   );
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import SidebarNavigation from './SidebarNavigations.js'
 import MainContainer from './MainContainer.js'
+import { Outlet } from 'react-router-dom'
 
 const Body = () => {
   return (
     <div className='size-11/12 w-full flex overflow-hidden'>
         <SidebarNavigation />
-        <MainContainer />
+        <Outlet />
     </div>
   )
 }
