1e9400929598958aead3d7fa3289c7b3abe9a102
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-04-27 00:45:17 +0530

    Watching video card

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {Youtube_API} from '../Constant.js'
 import VideoCard from './VideoCard'
+import { Link } from 'react-router-dom'
 const VideoList = () => {
   const [videosList, setVideoList] = useState([])
   useEffect(()=>{
@@ -23,7 +24,9 @@ const VideoList = () => {
     <div className='flex px-4 w-full flex-wrap gap-x-3 gap-y-10 overflow-auto'> 
         {
           videosList && videosList.map((videoData) => {
-              return <VideoCard key={videoData?.id} prop = {videoData}/>
+              return (
+                <Link to={'/watch?v=' + videoData?.id}><VideoCard key={videoData?.id} prop = {videoData}/></Link>
+            )
           })
         }
     </div>
--- src/components/WatchVideo.js
+++ src/components/WatchVideo.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const WatchVideo = () => {
   return (
-    <div><h1>Watch Video</h1></div>
+    <div className='flex'>
+      <div className='m-2 px-8 py-3 border-2 h-fit'>
+        <iframe width="980" height="550" src="https://www.youtube.com/embed/k4L-D-Q23Jw?si=ySn5PB8aicBAQGd5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      <div>
+        <h4>
+          Suggestions
+        </h4>
+      </div>
+    </div>
   )
 }
 
